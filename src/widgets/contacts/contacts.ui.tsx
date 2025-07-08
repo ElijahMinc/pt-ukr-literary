cd8651681aaa46b3994f34400777b900e9eea8fd
illia protsenko <illia.protsenko@fracturedbyte.com>
2025-07-08 18:09:02 +0100

    feat(contacts): synchronized contacts section with contentful content

--- src/widgets/contacts/contacts.ui.tsx
+++ src/widgets/contacts/contacts.ui.tsx
@@ -1,72 +1,68 @@
 import { Card } from '@/components/card/card.ui';
 import Image from 'next/image';
 import './contacts.scss';
+import client from '@/services/contentful';
+import { EntrySkeletonType } from 'contentful';
+import { IContactsFields } from '@/types/contentful';
+import { getDocumentToHtmlString } from '@/helpers/getDocumentToHtmlString';
+import { getContentfulImageData } from '@/services/contentful/helpers/getImageData';
 
-export const ContactsWidget = () => {
+export const ContactsWidget = async () => {
+  const contactsSectionData = await client.getEntries<EntrySkeletonType<IContactsFields>>({
+    content_type: 'contacts',
+    limit: 1,
+  });
+
+  const [contactsSection] = contactsSectionData.items;
+
+  const contactsData = contactsSection?.fields;
+
+  const title = contactsData.title;
+
+  const textBlock = contactsData?.textBlock ? getDocumentToHtmlString(contactsData?.textBlock) : '';
+
+  const image = contactsData?.image;
+
+  const imageUrl = getContentfulImageData(image, { image: true });
+  const imageTitle = getContentfulImageData(image, { title: true });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const mapSrc = `https://maps.google.com/maps?q=${(contactsData.location as any)?.lat},${(contactsData.location as any)?.lon}&z=15&output=embed`;
+
+  const isContentExist = !title || !imageUrl || !textBlock || contactsData?.location;
+
+  if (!isContentExist) return null;
+
   return (
     <section id='contacts' className='section-contacts'>
-      <h3 className='section-contacts__title'>Contacts</h3>
+      <h3 className='section-contacts__title'>{title}</h3>
 
       <div className='section-contacts__content'>
-        <div className='section-contacts__content-img'>
-          <Image src='/contacts-first-img.png' fill objectFit='cover' alt='about' />
-        </div>
+        {imageUrl && (
+          <div className='section-contacts__content-img'>
+            <Image src={`https:${imageUrl}`} fill objectFit='cover' alt={imageTitle || ''} />
+          </div>
+        )}
 
-        <Card className='section-contacts__content-card'>
-          <h4
-            className='card__title'
-            style={{
-              fontSize: '24px',
-            }}
-          >
-            Contact Sandbox:
-          </h4>
-          <p
-            className='card__description'
-            style={{
-              fontSize: '14px',
-            }}
-          >
-            +351 000000
-          </p>
-          <p
-            className='card__description'
-            style={{
-              fontSize: '14px',
-            }}
-          >
-            test@gmail.com
-          </p>
-          <h4
-            className='card__title'
-            style={{
-              marginTop: '30px',
-              fontSize: '24px',
-            }}
-          >
-            Address:
-          </h4>
-          <p
-            className='card__description'
-            style={{
-              fontSize: '14px',
-            }}
-          >
-            Rua Test Address, 99. Porto.
-          </p>
-        </Card>
+        {textBlock && (
+          <Card className='section-contacts__content-card'>
+            <div dangerouslySetInnerHTML={{ __html: textBlock }} />
+          </Card>
+        )}
 
-        <div className='section-contacts__content-map'>
-          <iframe
-            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3003.878667838742!2d-8.614771788491396!3d41.15899881001114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2464ff61baeb4f%3A0x147986203115d2f!2sR.%20de%20Antero%20de%20Quental%2089%2C%20Porto!5e0!3m2!1sru!2spt!4v1751648591843!5m2!1sru!2spt'
-            width='100%'
-            height='100%'
-            // style='border:0;'
-            // allowfullscreen=''
-            loading='lazy'
-            // referrerpolicy='no-referrer-when-downgrade'
-          ></iframe>
-        </div>
+        {contactsData?.location && (
+          <div className='section-contacts__content-map'>
+            <iframe
+              src={mapSrc}
+              width='100%'
+              height='100%'
+              // style='border:0;'
+              // allowfullscreen=''
+              loading='lazy'
+              // referrerpolicy='no-referrer-when-downgrade'
+            />
+          </div>
+        )}
       </div>
     </section>
   );
