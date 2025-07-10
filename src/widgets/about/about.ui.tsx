7338dea5ac3956d4d3998844abe9c04ecaa6218e
illia protsenko <illia.protsenko@fracturedbyte.com>
2025-07-10 01:38:13 +0100

    feat(about): synchronized about section with contenful

--- src/widgets/about/about.ui.tsx
+++ src/widgets/about/about.ui.tsx
@@ -1,48 +1,53 @@
 import { Card } from '@/components/card/card.ui';
 import Image from 'next/image';
 import './about.scss';
+import client from '@/services/contentful';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { EntrySkeletonType } from 'contentful';
+import { IAboutUsFields } from '@/types/contentful';
+import { getContentfulImageData } from '@/services/contentful/helpers/getImageData';
 
-export const AboutWidget = () => {
+export const AboutWidget = async () => {
+  const aboutSectionData = await client.getEntries<EntrySkeletonType<IAboutUsFields>>({
+    content_type: 'aboutUs',
+    limit: 1,
+  });
+
+  const [aboutSection] = aboutSectionData.items;
+
+  const aboutData = aboutSection?.fields;
+  const title = aboutData?.title;
+
+  const descriptionTitle = aboutData?.descriptionTitle ? aboutData?.descriptionTitle : '';
+
+  const Description = aboutData?.description
+    ? documentToReactComponents(aboutData?.description)
+    : '';
+
+  const image = aboutData?.mainImage;
+
+  const imageUrl = getContentfulImageData(image, { image: true });
+  const imageTitle = getContentfulImageData(image, { title: true });
+
+  const externalTitle = aboutData?.externalText ? aboutData?.externalText : '';
+
   return (
     <section className='section-about' id='about'>
-      <h3 className='section-about__title'>About us</h3>
+      <h3 className='section-about__title'>{title}</h3>
 
       <div className='section-about__content'>
         <div className='section-about__content-img'>
-          <Image src='/about-first-img.png' fill objectFit='cover' alt='about' />
+          <Image src={`https:${imageUrl}`} fill objectFit='cover' alt={imageTitle || ''} />
         </div>
 
         <Card
           className='section-about__content-card'
-          title='Souls language'
-          description={
-            <>
-              <span
-                style={{
-                  display: 'block',
-                }}
-              >
-                Poetry is the language of the soul — and this event is its gathering place. Our
-                mission is to bring together poets and lovers of poetry from Brazil, Portugal, and
-                Ukraine, celebrating rhythm, resistance, memory, and imagination.
-              </span>
-              <span
-                style={{
-                  display: 'block',
-                }}
-              >
-                In a single evening, cultures will meet, voices will rise, and hearts will find one
-                another through the magic of verse.
-              </span>
-            </>
-          }
+          title={descriptionTitle}
+          description={Description}
         />
       </div>
 
-      <div className='section-about__sub-content'>
-        There is no entrance fee — just bring your open heart. We welcome everyone: curious minds,
-        seasoned readers, quiet listeners.
-      </div>
+      <div className='section-about__sub-content'>{externalTitle}</div>
     </section>
   );
 };
