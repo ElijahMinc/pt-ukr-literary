ec1dbbb7f83abe39cc529b81af52d4c490f4bb97
illia protsenko <illia.protsenko@fracturedbyte.com>
2025-07-10 01:49:09 +0100

    feat(artists): synchronized artists with contentful

--- src/widgets/artists/artists.ui.tsx
+++ src/widgets/artists/artists.ui.tsx
@@ -1,16 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Artist } from '@/components/artist/artist.ui';
-import './artists.scss';
+import client from '@/services/contentful';
+import { EntrySkeletonType } from 'contentful';
+import { IArtistsFields } from '@/types/contentful';
 
-export const ArtistsWidget = () => {
+import './artists.scss';
+import { getContentfulImageData } from '@/services/contentful/helpers/getImageData';
+
+export const ArtistsWidget = async () => {
+  const artistsSectionData = await client.getEntries<EntrySkeletonType<IArtistsFields>>({
+    content_type: 'artists',
+    limit: 4,
+  });
+
+  const [artistsSection] = artistsSectionData.items;
+
+  const artistsData = artistsSection?.fields;
+
+  const artistsTitle = artistsData.artists;
+
+  const photos = artistsData.artistsPhotos as any[];
+
   return (
     <section id='artists' className='section-artists'>
-      <h3 className='section-artists__title'>Artists</h3>
+      <h3 className='section-artists__title'>{artistsTitle}</h3>
       <div className='section-artists__list'>
-        <Artist imageUrl='/artists-first-img.png' title='Leonardo da Vinci' />
-        <Artist imageUrl='/artists-first-img.png' title='Leonardo da Vinci' />
-        <Artist imageUrl='/artists-first-img.png' title='Leonardo da Vinci' />
-        <Artist imageUrl='/artists-first-img.png' title='Leonardo da Vinci' />
-        <Artist imageUrl='/artists-first-img.png' title='Leonardo da Vinci' />
+        {photos.map((data: any) => {
+          const imageUrl = getContentfulImageData(data, { image: true });
+          const imageTitle = getContentfulImageData(data, { title: true });
+          console.log('imageUrl', imageUrl);
+          return <Artist key={imageUrl} imageUrl={`https:${imageUrl}`} title={imageTitle || ''} />;
+        })}
       </div>
     </section>
   );
