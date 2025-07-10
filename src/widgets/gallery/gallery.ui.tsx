6b231a26efe0d9ec2c0c01c3e53d9eb3520e8d4b
illia protsenko <illia.protsenko@fracturedbyte.com>
2025-07-10 01:55:02 +0100

    feat(gallery): synchronized gallery with contacts

--- src/widgets/gallery/gallery.ui.tsx
+++ src/widgets/gallery/gallery.ui.tsx
@@ -1,46 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { GalleryCard } from '@/components/gallery-card/gallery-card.ui';
 import './gallery.scss';
+import client from '@/services/contentful';
+import { EntrySkeletonType } from 'contentful';
+import { IGalleryFields } from '@/types/contentful';
+import { getContentfulImageData } from '@/services/contentful/helpers/getImageData';
 
-export const GalleryWidget = () => {
+export const GalleryWidget = async () => {
+  const gallerySectionData = await client.getEntries<EntrySkeletonType<IGalleryFields>>({
+    content_type: 'gallery',
+    limit: 5,
+  });
+
+  const [gallerySection] = gallerySectionData.items;
+
+  const galleryData = gallerySection?.fields;
+
+  const galleryTitle = galleryData.Title;
+
+  const gallery = galleryData.gallery as any[];
+
   return (
     <section id='gallery' className='section-gallery'>
-      <h3 className='section-gallery__title'>Gallery</h3>
+      <h3 className='section-gallery__title'>{galleryTitle}</h3>
       <div className='section-gallery__list'>
-        <GalleryCard
-          imageUrl='/gallery-first-img.png'
-          altTitle='Leonardo da Vinci'
-          url='/gallery'
-          urlButtonText='Show more'
-          withHover
-        />
-        <GalleryCard
-          imageUrl='/gallery-first-img.png'
-          altTitle='Leonardo da Vinci'
-          url='/gallery'
-          urlButtonText='Show more'
-          withHover
-        />
-        <GalleryCard
-          imageUrl='/gallery-first-img.png'
-          altTitle='Leonardo da Vinci'
-          url='/gallery'
-          urlButtonText='Show more'
-          withHover
-        />
-        <GalleryCard
-          imageUrl='/gallery-first-img.png'
-          altTitle='Leonardo da Vinci'
-          url='/gallery'
-          urlButtonText='Show more'
-          withHover
-        />
-        <GalleryCard
-          imageUrl='/gallery-first-img.png'
-          altTitle='Leonardo da Vinci'
-          url='/gallery'
-          urlButtonText='Show more'
-          withHover
-        />
+        {gallery.map((data: any) => {
+          const imageUrl = getContentfulImageData(data, { image: true });
+          const imageTitle = getContentfulImageData(data, { title: true });
+
+          return (
+            <GalleryCard
+              key={imageUrl}
+              imageUrl={`https:${imageUrl}`}
+              altTitle={imageTitle || ''}
+              url='/gallery'
+              urlButtonText='Show more'
+              withHover
+            />
+          );
+        })}
       </div>
     </section>
   );
